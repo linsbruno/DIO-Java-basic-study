786a6b8090ee687fb1c6f26b1a2f293d55ffd83e
Bruno Lins <linslimabruno@gmail.com>
2024-05-16 13:55:45 -0300

    types and variables

---
+++ src/TypesVariables.java
@@ -0,0 +1,14 @@
+public class TypesVariables {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        short a = 1;
+        int b = 2;
+        float pi = 3.14f;
+        double c = 5.2 ;
+        long d = 1011033333121212121L;
+
+        String name = "Bruno Lins";
+
+        final double VALUE_OF_PI= 3.14;
+    }
+}
