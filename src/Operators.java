1f3ed64c3f0ae3db9ff30bc84bebd9df0cca3297
Bruno Lins <linslimabruno@gmail.com>
2024-05-16 14:09:11 -0300

    operators

---
+++ src/Operators.java
@@ -0,0 +1,23 @@
+
+import java.util.Date;
+
+public class Operators {
+    String nome = "Bruno";
+    int age = 30;
+    double weight = 110.5;
+    char sex = 'M';
+    boolean oraganDonor = true;
+    Date bornDate = new Date();
+
+
+    double sum = 10.5 + 15.7;
+    int sub = 113 - 25;
+    int mult = 20 * 7;
+    int div = 15 / 3;
+    int module = 18 % 3;
+    double result = ( 10 * 7 ) + ( 20 / 4 );
+
+    //attention "+" in String is concat
+
+    String completeName ="Bruno " + "Lins";
+}
